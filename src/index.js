72b10436fc728ff632d04ca2b44ac0713a96c745
Mike <nhj25252@gmail.com>
2023-04-25 21:10:16 +0900

    refactor: use object destructuring

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import "./styles.css";
 
 // key 7d3bc585323a47ed8b482529232204
@@ -11,65 +12,79 @@ import "./styles.css";
 
 const searchBar = document.querySelector(".search-bar");
 
-async function getCurrentWeather() {
-  // current weather
-  const searchValue = searchBar.value;
-
-  try {
-    const URL = `https://api.weatherapi.com/v1/current.json?key=7d3bc585323a47ed8b482529232204&q=${searchValue}`;
-    const response = await fetch(URL, { mode: "cors" });
-    const data = await response.json();
-    console.log(data);
-
-    console.log();
-  } catch (error) {
-    console.error(error); // couldn't fetch API?
-  }
-}
-
 async function getForecast() {
   const searchValue = searchBar.value;
 
-  const URL = `https://api.weatherapi.com/v1/forecast.json?key=7d3bc585323a47ed8b482529232204&q=${searchValue}&alerts=yes&aqi=yes`;
+  const URL = `https://api.weatherapi.com/v1/forecast.json?key=7d3bc585323a47ed8b482529232204&q=${searchValue}&days=3&alerts=yes&aqi=yes`;
 
   try {
-    const response = await fetch(URL, { mode: "cors" });
+    const response = await fetch(URL, { mode: "cors" }); // fetch only throws an error if there's disconnection
     const data = await response.json();
-    console.log(data); // if error in data, logs error
-    console.log(data.location.name);
-    console.log(data.location.country);
-    console.log(data.location.localtime);
+    console.log(data);
 
-    console.log(data.current.condition.text); // partly cloudy
-    console.log(data.current.temp_c);
-    console.log(data.current.feelslike_c);
-    console.log(data.current.temp_f);
-    console.log(data.current.feelslike_f);
-    console.log(data.current.humidity);
+    const { country, localtime, name: city } = data.location;
 
-    // forecast
-    console.log(data.forecast.forecastday);
-    console.log(data.forecast.forecastday[0].astro);
-    console.log(data.forecast.forecastday[0].astro.moonrise);
-    console.log(data.forecast.forecastday[0].astro.moon_phase);
-    console.log(data.forecast.forecastday[0].astro.sunrise);
-    console.log(data.forecast.forecastday[0].astro.sunset);
+    const current_condition = data.current.condition.text;
+    const {
+      temp_c,
+      feelslike_c,
+      temp_f,
+      feelslike_f,
+      humidity: current_humidity,
+    } = data.current;
 
-    console.log(data.forecast.forecastday.daily_chance_of_rain);
-    console.log(data.forecast.forecastday.daily_chance_of_snow);
-    console.log(data.forecast.forecastday.maxtemp_c);
-    console.log(data.forecast.forecastday.mintemp_c);
-    console.log(data.forecast.forecastday.maxtemp_f);
-    console.log(data.forecast.forecastday.mintemp_f);
+    // forecast for day - astro
+    const { moonrise, moon_phase, sunrise, sunset } =
+      data.forecast.forecastday[0].astro;
+
+    const conditionForDay = data.forecast.forecastday[0].day.condition.text;
+    const {
+      daily_chance_of_rain,
+      daily_chance_of_snow,
+      avghumidity,
+      avgtemp_c,
+      avgtemp_f,
+      maxtemp_c,
+      mintemp_c,
+      maxtemp_f,
+      mintemp_f,
+    } = data.forecast.forecastday[0].day;
+
+    return {
+      city,
+      country,
+      localtime,
+      current_condition,
+      temp_c,
+      feelslike_c,
+      temp_f,
+      feelslike_f,
+      current_humidity,
+      moonrise,
+      moon_phase,
+      sunrise,
+      sunset,
+      conditionForDay,
+      daily_chance_of_rain,
+      daily_chance_of_snow,
+      avgtemp_c,
+      avgtemp_f,
+      avghumidity,
+      maxtemp_c,
+      mintemp_c,
+      maxtemp_f,
+      mintemp_f,
+    };
   } catch (error) {
     console.log(error.message);
   }
+  return null;
 }
 
 searchBar.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     e.preventDefault();
-    // getCurrentWeather();
     getForecast();
+    console.log(getForecast());
   }
 });
